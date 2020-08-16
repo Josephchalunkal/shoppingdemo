df23100d8b282fcba53a33b2d4fe0193f814a1c7
Josephchalunkal <54778434+Josephchalunkal@users.noreply.github.com>
2020-08-17 00:57:10 +0530

    Updates

---
+++ precache-manifest.be03fc082a739809406c3e5ce1edd15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797de82dc18e26a5ae90cb4d5c8d2486",
+    "url": "/shoppingdemo/index.html"
+  },
+  {
+    "revision": "035c2d0f529117d06ec3",
+    "url": "/shoppingdemo/static/css/main.97d81ea7.chunk.css"
+  },
+  {
+    "revision": "a7ad71cf589652205074",
+    "url": "/shoppingdemo/static/js/2.c5294eab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingdemo/static/js/2.c5294eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035c2d0f529117d06ec3",
+    "url": "/shoppingdemo/static/js/main.2c177346.chunk.js"
+  },
+  {
+    "revision": "69f7cca32d93bd271c96",
+    "url": "/shoppingdemo/static/js/runtime-main.f9ecd561.js"
+  }
+]);
